d7c9c67f3bc76a7f0c43a4909d7711c04f9b19a8
mooosmad <mooosmad@gmail.com>
2022-09-11 16:02:10 +0000

    add signin page

--- igname_li/lib/theme/choosetheme.dart
+++ igname_li/lib/theme/choosetheme.dart
@@ -29,146 +29,168 @@ class _ChooseThemePageState extends State<ChooseThemePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            final prefs = await SharedPreferences.getInstance();
-            prefs.setBool('isFirst', false);
-            changeTheme(Get.isDarkMode);
-            Get.off(const AuthenticationPage());
-          },
-          child: const Icon(Icons.arrow_forward_rounded),
-        ),
-        body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: ListView(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 2,
-                child: Lottie.asset(
-                  "assets/lotties/darkmode.json",
-                  frameBuilder: ((context, child, composition) {
-                    return composition != null
-                        ? child
-                        : Container(
-                            height: MediaQuery.of(context).size.height / 2,
-                          );
-                  }),
-                  reverse: true,
-                ),
-              ),
-              Text(
-                "Mode Sombre",
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "Choisissez le theme qui vous combien le mieux",
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyText2!.copyWith(
-                      color: Colors.grey,
-                    ),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(
-                    5,
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    // GestureDetector(
-
-                    OutlinedButton(
-                      onPressed: () {
-                        Get.changeThemeMode(
-                          Get.isDarkMode ? ThemeMode.light : ThemeMode.dark,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final prefs = await SharedPreferences.getInstance();
+          prefs.setBool('isFirst', false);
+          changeTheme(Get.isDarkMode);
+          Get.off(const AuthenticationPage());
+        },
+        child: const Icon(Icons.arrow_forward_rounded),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 2,
+              child: Lottie.asset(
+                "assets/lotties/darkmode.json",
+                frameBuilder: ((context, child, composition) {
+                  return composition != null
+                      ? child
+                      : Container(
+                          height: MediaQuery.of(context).size.height / 2,
                         );
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.all(16),
-                        // padding: const EdgeInsets.all(3.0),
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(12)),
-                            border: Border.all(
-                              color: Colors.white,
-                              width: 2.5,
-                            )),
-                        height: 180,
-                        width: 80,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            const Icon(
-                              Icons.light_mode,
-                              color: Colors.white,
-                            ),
-                            Text("Aa",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .headline1!
-                                    .copyWith(color: Colors.white)),
-                          ],
-                        ),
-                      ),
-                    ),
-
-                    Container(
-                      margin: const EdgeInsets.all(16),
-                      height: 180,
-                      width: 80,
-                      decoration: BoxDecoration(
-                        color: Colors.black26,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(12)),
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 2.5,
-                        ),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          const Icon(
-                            Icons.dark_mode,
-                            color: Colors.black,
-                          ),
-                          Text(
-                            "Aa",
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline1!
-                                .copyWith(color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                }),
+                reverse: true,
               ),
-              // Switch(
-              //   activeColor: const Color(0xfffdc72f),
-              //   value: isdarkmode,
-              //   onChanged: (v) async {
-              //     final prefs = await SharedPreferences.getInstance();
-              // prefs.setBool('isFisrt', false);
-              // setState(() {
-              //   isdarkmode = v;
-              // });
-
-              //     Get.changeThemeMode(
-              //       Get.isDarkMode ? ThemeMode.light : ThemeMode.dark,
-              //     );
-              //   },
-              // )
-            ],
-          ),
-        ));
+            ),
+            Text(
+              "Mode Sombre",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Choisissez le theme qui vous combien le mieux",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                    color: Colors.grey,
+                  ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(8),
+                ),
+                color: Get.isDarkMode ? Colors.black26 : Colors.grey.shade200,
+              ),
+              height: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          maximumSize: const Size(120, 150),
+                          backgroundColor: const Color(0xffe5e5e5),
+                        ),
+                        onPressed: () {
+                          Get.changeThemeMode(
+                            Get.isDarkMode ? ThemeMode.light : ThemeMode.dark,
+                          );
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                            left: 10,
+                            top: 10,
+                          ),
+                          padding: const EdgeInsets.all(8),
+                          // color: Colors.white,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(8),
+                              topRight: Radius.circular(8),
+                            ),
+                            border: Border.all(
+                              color: Colors.grey.shade400,
+                              width: 1,
+                            ),
+                          ),
+                          height: 80,
+                          width: 70,
+                          child: Text("Aa",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline1!
+                                  .copyWith(color: Colors.black)),
+                        ),
+                      ),
+                      OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          maximumSize: const Size(120, 150),
+                          backgroundColor: const Color(0xff626262),
+                        ),
+                        onPressed: () {
+                          Get.changeThemeMode(
+                            Get.isDarkMode ? ThemeMode.light : ThemeMode.dark,
+                          );
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                            left: 10,
+                            top: 10,
+                          ),
+                          padding: const EdgeInsets.all(8),
+                          // color: Colors.white,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(8),
+                              topRight: Radius.circular(8),
+                            ),
+                            border: Border.all(
+                              color: Colors.grey.shade500,
+                              width: 1,
+                            ),
+                          ),
+                          height: 80,
+                          width: 70,
+                          child: Text(
+                            "Aa",
+                            style:
+                                Theme.of(context).textTheme.headline1!.copyWith(
+                                      color: Colors.white,
+                                    ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text("Clair",
+                          style: Theme.of(context).textTheme.bodyText1),
+                      Text("Sombre",
+                          style: Theme.of(context).textTheme.bodyText1),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            // Switch(
+            //   activeColor: const Color(0xfffdc72f),
+            //   value: isdarkmode,
+            //   onChanged: (v) async {
+            //     final prefs = await SharedPreferences.getInstance();
+            // prefs.setBool('isFisrt', false);
+            // setState(() {
+            //   isdarkmode = v;
+            // });
+            //     Get.changeThemeMode(
+            //       Get.isDarkMode ? ThemeMode.light : ThemeMode.dark,
+            //     );
+            //   },
+            // )
+          ],
+        ),
+      ),
+    );
   }
 }
